2244390dd0c425e85141674eaa4b63f0586dd5ef
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2023-07-07 00:35:21 +0100

    Update check_images.py
    Implemented the time function

--- check_images.py
+++ check_images.py
@@ -40,7 +40,7 @@ from print_results import print_results
 # Main program function defined below
 def main():
     # TODO 0: Measures total program runtime by collecting start time
-    start_time = time()
+    start_time = time.time()
     
     # TODO 1: Define get_input_args function within the file get_input_args.py
     # This function retrieves 3 Command Line Arugments from user as input from
@@ -116,7 +116,7 @@ def main():
     print_results(results, results_stats, in_arg.arch, True, True)
     
     # TODO 0: Measure total program runtime by collecting end time
-    end_time = time()
+    end_time = time.time()
     
     # TODO 0: Computes overall runtime in seconds & prints it in hh:mm:ss format
     tot_time = start_time - end_time #calculate difference between end time and start time
